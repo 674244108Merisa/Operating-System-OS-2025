d25862750d1a0a2f24d10d3e98806e59276a9b82
674244108Merisa <674244108@mail.pbru.ac.th>
2025-10-08 13:25:42 +0700

    Add files via upload

---
+++ chapter1/child.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+main() {
+	printf("My bio.: \n");
+	printf("ID : 674244108 \n");
+	printf("Name :Merisa Suwanpanich\n");
+	printf("------------------------------\n");
+	getchar();
+}
